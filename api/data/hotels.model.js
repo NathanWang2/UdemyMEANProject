41e9d571ffb4cfabf56b57b675ad1b90d5a40da5
nw341 <nathan.wang2@gmail.com>
2017-05-16 22:01:26 -0500

    Added new schemas for sub-documents and referenced them in the main schema. Also added the ability to read geospachial json data by indexing them in a 2dsphere

--- api/data/hotels.model.js
+++ api/data/hotels.model.js
@@ -1,21 +1,66 @@
-    var mongoose = require('mongoose');
+var mongoose = require('mongoose');
+// Nested doc Schema should be before the main
 
-    var hotelSchema = new mongoose.Schema({
-        name: {
-            type: String,
-            required: true
-        },
-        stars: {
-            type: Number,
-            min: 0,
-            max: 5,
-            default: 0
-        },
-        services: [String], // This is an array [] of strings
-        description: String,
-        photos: [String],
-        currency: String,
-    });
+// create seperate schema for sub documents
+// after creating, refrence this in the main schema
+var reviewSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        min: 0,
+        mac: 5,
+        required: true
+    },
+    review: {
+        type: String,
+        required: true
+    },
+    createdOn: {
+        type: Date,
+        "default": Date.now
+    }
+});
+
+// Another sub Schema
+var roomSchema = new mongoose.Schema({
+    type: String,
+    number: Number,
+    description: String,
+    photos: [String],
+    price: Number
+});
+
+var hotelSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    stars: {
+        type: Number,
+        min: 0,
+        max: 5,
+        default: 0
+    },
+    services: [String], // This is an array [] of strings
+    description: String,
+    photos: [String],
+    currency: String,
+    review: [reviewSchema], // Refrencing the reviewSchema
+    rooms: [roomSchema],
+    location: {
+        address: String,
+        // geospachial json data storage
+        // Always longitude (E/W) then latitude (N/S)
+        // To allow tracking, we need to index this
+        coordinates: {
+            type: [Number],
+            index: "2dsphere"
+        }
+    }
+});
 
 // 1st param is name of model
 // 2nd param is name of Schema
