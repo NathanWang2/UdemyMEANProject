5ba1384266b13e80f9e748e951b7eb07933ca80a
nw341 <nathan.wang2@gmail.com>
2017-05-24 10:29:17 -0500

    Trapped for errors in querystrings, errors from mongoose operations, protected against misuse of the API, errors from query paramaters, and alternatives for code

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -30,25 +30,46 @@ var runGeoQuery = function(req, res){
 module.exports.hotelsGetAll = function(req, res){
 
     // default values
-        var offset = 0;
-        var count = 5;
+    var offset = 0;
+    var count = 5;
+    var maxCount = 10;
 
-        // if the query exists and there is a latitude and a longitude
-        if (req.query && req.query.lat && req.query.lng){
-            runGeoQuery(req, res);
-            return; // Stops code if you hit this spot
-        }
+    // if the query exists and there is a latitude and a longitude
+    if (req.query && req.query.lat && req.query.lng){
+        runGeoQuery(req, res);
+        return; // Stops code if you hit this spot
+    }
 
     // If a query property exists and...
     // If it does exist, then check if there is an offset and set the data
-        if (req.query && req.query.offset){
-            // Make it an int instead of a string
-            offset = parseInt(req.query.offset, 10);
-        }
-        if (req.query && req.query.count){
-            // Make it an int instead of a string
-            count = parseInt(req.query.count, 10);
-        }
+    if (req.query && req.query.offset){
+        // Make it an int instead of a string
+        offset = parseInt(req.query.offset, 10);
+    }
+    if (req.query && req.query.count){
+        // Make it an int instead of a string
+        count = parseInt(req.query.count, 10);
+    }
+
+    // If offset or count is not a number end
+    if (isNaN(offset) || isNaN(count)){
+        res
+            .status(400)
+            .json({
+                "message" : "If supplied in querystring count and offset should"
+                + " be numbers"
+            });
+        return;
+    }
+
+    if (count > maxCount){
+        res
+            .status(400)
+            .json({
+                "message" : "Count limit of " + maxCount + " exceeded"
+            });
+        return;
+    }
 
     Hotel
         .find()
@@ -56,10 +77,18 @@ module.exports.hotelsGetAll = function(req, res){
         .limit(count)
         // error, return data.
         .exec(function(err, hotels){
-            // Return how many hotels were found an respond with hotels
-            console.log("Found hotels", hotels.length);
-            res
-               .json(hotels);
+            if (err){
+                console.log("Error finding hotels");
+                res
+                    .status(500)
+                    .json(err);
+            }
+            else {
+                // Return how many hotels were found an respond with hotels
+                console.log("Found hotels", hotels.length);
+                res
+                   .json(hotels);
+            }
         });
 };
 
@@ -68,12 +97,32 @@ module.exports.hotelsGetOne = function(req, res){
     // request the url paramater and save it in hotelID
     var hotelId = req.params.hotelId;
     console.log("Get the hotelID", hotelId);
+
     Hotel
         .findById(hotelId)
         .exec(function(err, doc){
+            // Cleaner to make an object response and respond at the end of
+            // function
+            var response = { // default
+                status : 200,
+                message : doc
+            };
+
+            if (err){
+                console.log("Error finding hotel");
+                response.status = 500;
+                response.message = err;
+            }
+            // If the document is not found (Does not exist)
+            else if(!doc) {
+                response.status = 404;
+                response.message = {
+                        "message" : "Hotel ID not found"
+                    };
+            }
             res
-                .status(200)
-                .json(doc);
+                .status(response.status)
+                .json(response.message);
     });
 };
 
